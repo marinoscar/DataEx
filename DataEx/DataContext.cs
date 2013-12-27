a5f36456b2ff90a392aeea369c19bf6ab12ef900
Oscar Marin <oscar.marin@entegreat.com>
2013-12-26 22:40:25 -0600

    Added DataContext classes

---
+++ DataEx/DataContext.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataEx
+{
+    public class DataContext
+    {
+
+        public DataContext()
+        {
+            Items = new Dictionary<Type, object>();
+        }
+
+        protected Dictionary<Type, object> Items { get; private set; }
+
+        private DataList<T> GetDataList<T>()
+        {
+            var type = typeof (T);
+            if (!Items.ContainsKey(type))
+                Items[type] = new DataList<T>();
+            return (DataList<T>)Items[type];
+        } 
+
+        public void Set<T>(T item)
+        {
+            GetDataList<T>().Add(item);
+        } 
+        public IQueryable<T> Get<T>()
+        {
+            return GetDataList<T>();
+        } 
+
+        public void Remove<T>(T item)
+        {
+            GetDataList<T>().Remove(item);
+        }
+
+        public virtual int SaveChanges()
+        {
+            return 0;
+        }
+    }
+}
